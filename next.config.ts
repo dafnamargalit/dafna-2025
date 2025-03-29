945fdac4301be074be590963f11a43cbb0926508
Dafna <margalit.dafna77@gmail.com>
2025-03-29 14:00:42 -0100

    hopefully fix config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,31 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(glb|gltf)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          publicPath: '/_next/static/',
+          outputPath: 'static/',
+        },
+      },
+    });
+    return config;
+  },
+  experimental: {
+    serverActions: true,
+  },
+  images: {
+    domains: ['teck.s3.us-east-1.amazonaws.com'],
+  },
+  // Increase the maximum payload size for API routes
+  api: {
+    bodyParser: {
+      sizeLimit: '10mb',
+    },
+  },
 };
 
 export default nextConfig;
