b5c34fa3f0deedf190578830cf998f497ca23256
Dafna <margalit.dafna77@gmail.com>
2025-10-08 13:11:43 -0700

    Update Announcement.tsx

--- src/components/Announcement.tsx
+++ src/components/Announcement.tsx
@@ -7,11 +7,11 @@ interface AnnouncementProps {
 export default function Announcement({ isMobile }: AnnouncementProps) {
   return (
     <Link 
-      href={'https://ffm.to/dafna-shittalking'} 
+      href={'https://ffm.to/dafna-controlledchaos'} 
       className={`hover:opacity-70 bg-cyan-300 flex ${isMobile ? 'mt-40' : ''} items-center justify-center ${isMobile ? 'p-1 text-sm' : 'p-2'} border-solid border-2 border-cyan-500 text-cyan-700 cursor-pointer`}
-      aria-label="STREAM SH!TTALKING"
+      aria-label="PRESAVE CONTROLLEDCHAOS"
     >
-      <i>STREAM SH!TTALKING</i>
+      <i>PRESAVE CONTROLLEDCHAOS</i>
     </Link>
   );
 }
