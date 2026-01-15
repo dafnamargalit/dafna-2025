131a3d5cabbcb87bc83beaf55566f827d1d539fc
Dafna <margalit.dafna77@gmail.com>
2026-01-14 16:54:23 -0800

    upgrade

--- src/components/FlatSite/Layout.tsx
+++ src/components/FlatSite/Layout.tsx
@@ -22,18 +22,18 @@ export default function Layout({ children }: LayoutProps) {
   return (
     <div className={`relative min-h-screen bg-black ${retroFont.className}`}>
       {/* Fixed background */}
-      <div className="fixed inset-0 z-0">
+      <div className="fixed inset-0 z-0 pointer-events-none">
         <TunnelSceneContent />
       </div>
       
       {/* Content overlay */}
-      <div className="relative z-10">
+      <div className="relative z-10 min-h-[100dvh] overflow-y-auto touch-pan-y">
         {children}
       </div>
 
       {/* Loading overlay */}
       {!isTunnelLoaded && (
-        <div className="fixed inset-0 z-50 bg-black/50 flex items-center justify-center">
+        <div className="fixed inset-0 z-50 bg-black/50 flex items-center justify-center pointer-events-none">
           <LoadingIndicator />
         </div>
       )}
